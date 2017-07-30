72055cd1653d05c6276deae9d97205ab049173a3
Sander de Witte <github@sanderdewitte.com>
2017-07-30 20:21:33 +0200

    Update rancheros-cloud-init.sh

--- rancheros-cloud-init.sh
+++ rancheros-cloud-init.sh
@@ -9,20 +9,6 @@ hostname: $HOSTNAME
 ssh_authorized_keys:
   - ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAD6hltyl1MpRm6Q2KWr2QwaPGwa2RgGvyQh1u7Fgl+BsHJZiwmjhBMVdwH+CfJ3dD9m2cTnDXqdYJF5qfUl55DOsQHRYaqBywpv3bQ6LF+nJQNKSA0/BJJl2ONUWdQ7LmcUJmD6QtsKEY1JQEvRUtr6KfShokN7hYW0fn47HeolqlKQkA==
 write_files:
-  - container: ntp
-    path: /etc/ntp.conf
-    permissions: "0644"
-    owner: root:root
-    content: |
-      server 0.pool.ntp.org iburst
-      server 1.pool.ntp.org iburst
-      server 2.pool.ntp.org iburst
-      server 3.pool.ntp.org iburst
-      # Allow only time queries, at a limited rate, sending KoD when in excess.
-      # Allow all local queries (IPv4, IPv6)
-      restrict default nomodify nopeer noquery limited kod
-      restrict 127.0.0.1
-      restrict [::1]
   - path: /etc/ssh/sshd_config
     permissions: "0600"
     owner: root:root
