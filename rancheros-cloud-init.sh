2934e6a68f16e4c19308bedeb2a643ec9da56e71
Sander de Witte <github@sanderdewitte.com>
2017-07-30 21:40:44 +0200

    Update rancheros-cloud-init.sh
    Added Vultr specific items

--- rancheros-cloud-init.sh
+++ rancheros-cloud-init.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+VULTR_API_URL=https://api.vultr.com
+VULTR_API_VER=v1
+VULTR_API_KEY=<VULTR_API_KEY>
+VULTR_IP_V4="$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' | xargs echo -n $1)"
+VULTR_HOST=$(curl ${VULTR_API_URL}/${VULTR_API_VER}/server/list?api_key=${VULTR_API_KEY} | jq ".[] | select(.main_ip==\"${VULTR_IP_V4}\") | .label" | xargs echo -n $1)
+
 cat > "cloud-config.yml" <<EOF
 #cloud-config
+hostname: $HOSTNAME
 ssh_authorized_keys:
   - ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAD6hltyl1MpRm6Q2KWr2QwaPGwa2RgGvyQh1u7Fgl+BsHJZiwmjhBMVdwH+CfJ3dD9m2cTnDXqdYJF5qfUl55DOsQHRYaqBywpv3bQ6LF+nJQNKSA0/BJJl2ONUWdQ7LmcUJmD6QtsKEY1JQEvRUtr6KfShokN7hYW0fn47HeolqlKQkA==
 write_files:
