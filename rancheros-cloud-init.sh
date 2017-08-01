567fd19a5bac005cd4b39a33590c01e26d2da0d5
Sander de Witte <github@sanderdewitte.com>
2017-08-01 23:58:21 +0200

    Update rancheros-cloud-init.sh

--- rancheros-cloud-init.sh
+++ rancheros-cloud-init.sh
@@ -4,8 +4,20 @@ VULTR_API_KEY=""
 VULTR_API_URL=https://api.vultr.com
 VULTR_API_VER=v1
 
-VULTR_SVR_CREATION_DATES=$(wget -qO- --header="API-Key: ${VULTR_API_KEY}" ${VULTR_API_URL}/${VULTR_API_VER}/server/list | grep -Po "\"date_created\":\"\K\d{4}(?:-\d{2}){2}\s+(?:\d{2}:){2}\d{2}")
-VULTR_SVR_CREATION_DATES_ARRAY=(`echo $SERVER_CREATION_DATES | sed -re 's/(-[0-9]{2})[[:space:]]{1,}([0-9]{2}:)/\1T\2/g'`)
+VULTR_SVR_CREATION_DATETIME_LIST=$(wget -qO- --header="API-Key: ${VULTR_API_KEY}" ${VULTR_API_URL}/${VULTR_API_VER}/server/list | grep -Po "\"date_created\":\"\K\d{4}(?:-\d{2}){2}\s+(?:\d{2}:){2}\d{2}")
+VULTR_SVR_CREATION_DATETIMES=($(echo $VULTR_SVR_CREATION_DATETIME_LIST | sed -re 's/(-[0-9]{2})[[:space:]]{1,}([0-9]{2}:)/\1T\2/g'))
+
+IDX=0
+MAX_IDX=0
+MAX_VULTR_SVR_CREATION_TIMESTAMP=$(date --date=${VULTR_SVR_CREATION_DATETIMES[0]} +%s)
+for VULTR_SVR_CREATION_DATETIME in ${VULTR_SVR_CREATION_DATETIMES[@]}; do
+  VULTR_SVR_CREATION_TIMESTAMP=$(date --date=${VULTR_SVR_CREATION_DATETIME} +%s)
+  expr $VULTR_SVR_CREATION_TIMESTAMP \> $MAX_VULTR_SVR_CREATION_TIMESTAMP > /dev/null && { MAX_IDX=$IDX; MAX_VULTR_SVR_CREATION_TIMESTAMP=$VULTR_SVR_CREATION_TIMESTAMP; }
+  : $((IDX++))
+done
+LAST_BUILT_VULTR_SVR_CREATION_DATETIME=${VULTR_SVR_CREATION_DATETIMES[$MAX_IDX]/T/ }
+
+SUBID=...
 
 VULTR_PVT_IPV4=$(wget -qO- --header="API-Key: ${VULTR_API_KEY}" --header="Label: ${VULTR_SVR_LBL}" ${VULTR_API_URL}/${VULTR_API_VER}/server/list | grep -Po "\"internal_ip\":\"\K(?:\d{1,3}\.){3}\d{1,3}")
 
