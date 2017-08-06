092c188fff71c7b615cca614fb53d7a99e160b42
Sander de Witte <github@sanderdewitte.com>
2017-08-06 23:40:42 +0200

    Update rancheros-cloud-init.sh

--- rancheros-cloud-init.sh
+++ rancheros-cloud-init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # API information
-API_KEY="XMZSERJNBBR3DJA63ZHJKP26FIYJBOSIBZ5A"
+API_KEY=""
 API_URL="https://api.vultr.com"
 API_VER="v1"
 
@@ -65,7 +65,7 @@ cat > "cloud-config.yml" <<EOF
 #cloud-config
 hostname: $LAST_BUILT_SVR_LABEL
 ssh_authorized_keys:
-  - ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAD6hltyl1MpRm6Q2KWr2QwaPGwa2RgGvyQh1u7Fgl+BsHJZiwmjhBMVdwH+CfJ3dD9m2cTnDXqdYJF5qfUl55DOsQHRYaqBywpv3bQ6LF+nJQNKSA0/BJJl2ONUWdQ7LmcUJmD6QtsKEY1JQEvRUtr6KfShokN7hYW0fn47HeolqlKQkA==
+  - ecdsa-sha2-nistp521 ...
 write_files:
   - path: /etc/ssh/sshd_config
     permissions: "0600"
