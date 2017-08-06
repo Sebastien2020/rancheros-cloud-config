3da66f5f497bd667b86b201b16935117cf8c4f5c
Sander de Witte <github@sanderdewitte.com>
2017-08-06 22:40:52 +0200

    Update rancheros-cloud-init.sh

--- rancheros-cloud-init.sh
+++ rancheros-cloud-init.sh
@@ -63,7 +63,7 @@ fi
 # Generate cloud-config
 cat > "cloud-config.yml" <<EOF
 #cloud-config
-hostname: $LAST_BUILT_SRV_LABEL
+hostname: ${LAST_BUILT_SRV_LABEL}
 ssh_authorized_keys:
   - ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAD6hltyl1MpRm6Q2KWr2QwaPGwa2RgGvyQh1u7Fgl+BsHJZiwmjhBMVdwH+CfJ3dD9m2cTnDXqdYJF5qfUl55DOsQHRYaqBywpv3bQ6LF+nJQNKSA0/BJJl2ONUWdQ7LmcUJmD6QtsKEY1JQEvRUtr6KfShokN7hYW0fn47HeolqlKQkA==
 write_files:
